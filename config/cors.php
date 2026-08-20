ff9d59a1ebd7c77516269316deecf18184a9e10c
Dede <dedemahendra201@gmail.com>
2026-08-20 21:43:37 +0700

    fix: configure sanctum csrf for production

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'], // domain.com, namakalian.com
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
 
